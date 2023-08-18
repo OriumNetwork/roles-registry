36749b4906d964884f290a2a3003c0d21e8050a1
Daniel Lima <daniel.lima@orium.network>
2023-08-18 13:19:53 -0300

    ON-458: Deployment with AWS KMS

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,30 +1,111 @@
+import * as dotenv from 'dotenv'
 import 'solidity-coverage'
 import 'hardhat-gas-reporter'
 import '@nomiclabs/hardhat-etherscan'
 import '@openzeppelin/hardhat-upgrades'
 import 'hardhat-spdx-license-identifier'
 import '@nomicfoundation/hardhat-toolbox'
+import '@openzeppelin/hardhat-defender'
 
-module.exports = {
+dotenv.config()
+
+const {
+  ENVIRONMENT,
+  COINMARKETCAP_API_KEY,
+  DEV_PRIVATE_KEY,
+  PROD_PRIVATE_KEY,
+  DEFENDER_TEAM_API_KEY,
+  DEFENDER_TEAM_API_SECRET_KEY,
+  POLYGONSCAN_API_KEY,
+  ETHER_SCAN_API_KEY,
+  POLYGON_PROVIDER_URL,
+  MUMBAI_PROVIDER_URL,
+  GOERLI_PROVIDER_URL,
+} = process.env
+
+const BASE_CONFIG = {
   solidity: {
-    version: '0.8.9',
-    optimizer: {
-      enabled: true,
-      runs: 200,
-    },
+    compilers: [
+      {
+        version: '0.8.9',
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+      {
+        version: '0.5.10',
+        optimizer: {
+          enabled: true,
+          runs: 200,
+        },
+      },
+    ],
   },
   mocha: {
     timeout: 840000,
   },
   gasReporter: {
-    enabled: true,
-    excludeContracts: ['contracts/mocks'],
+    enabled: false,
+    excludeContracts: ['contracts/test'],
     gasPrice: 100,
     token: 'MATIC',
     currency: 'USD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
   },
   spdxLicenseIdentifier: {
     overwrite: false,
     runOnCompile: true,
   },
+  etherscan: {
+    apiKey: {
+      polygon: POLYGONSCAN_API_KEY,
+      polygonMumbai: POLYGONSCAN_API_KEY,
+      goerli: ETHER_SCAN_API_KEY,
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: POLYGON_PROVIDER_URL,
+        blockNumber: 45752368,
+      },
+    },
+  },
 }
+
+const PROD_CONFIG = {
+  ...BASE_CONFIG,
+  defender: {
+    apiKey: DEFENDER_TEAM_API_KEY,
+    apiSecret: DEFENDER_TEAM_API_SECRET_KEY,
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: POLYGON_PROVIDER_URL,
+        blockNumber: 45752368,
+      },
+    },
+    mumbai: {
+      chainId: 80001,
+      url: MUMBAI_PROVIDER_URL,
+      accounts: [DEV_PRIVATE_KEY],
+    },
+    polygon: {
+      chainId: 137,
+      url: POLYGON_PROVIDER_URL,
+      accounts: [PROD_PRIVATE_KEY],
+    },
+    goerli: {
+      chainId: 5,
+      url: GOERLI_PROVIDER_URL,
+      accounts: [DEV_PRIVATE_KEY],
+    },
+  },
+}
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = ENVIRONMENT === 'prod' ? PROD_CONFIG : BASE_CONFIG
---
+++ scripts/01-deploy.ts
@@ -0,0 +1,43 @@
+import { ethers, network as hardhatNetwork } from 'hardhat'
+import { AwsKmsSigner } from '@govtechsg/ethers-aws-kms-signer'
+
+const kmsCredentials = {
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || 'AKIAxxxxxxxxxxxxxxxx', // credentials for your IAM user with KMS access
+  secretAccessKey: process.env.AWS_ACCESS_KEY_SECRET || 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // credentials for your IAM user with KMS access
+  region: 'us-east-1', // region of your KMS key
+  keyId: process.env.AWS_KMS_KEY_ID || 'xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', // KMS key id
+}
+
+const provider = new ethers.providers.JsonRpcProvider(process.env.GOERLI_PROVIDER_URL || '')
+const signer = new AwsKmsSigner(kmsCredentials).connect(provider)
+
+const NETWORK = hardhatNetwork.name
+const CONTRACT_NAME = 'RolesRegistry'
+
+async function main() {
+  const operator = await signer.getAddress()
+  console.log(`Deploying ${CONTRACT_NAME} contract on: ${NETWORK} network with ${operator}`)
+
+  const ContractFactory = await ethers.getContractFactory(CONTRACT_NAME)
+  const create2Factory = await ethers.getContractAt(
+    'ImmutableCreate2Factory',
+    '0x0000000000FFe8B47B3e2130213B802212439497', // This address is the same on all networks
+    signer,
+  )
+
+  const bytecode = ContractFactory.bytecode
+  const salt = '0x00000000000000000000000000000000000000008b99e5a778edb02572010000'
+
+  const tx = await create2Factory.safeCreate2(salt, bytecode)
+
+  console.log(`${CONTRACT_NAME} deployment txHash: ${tx.hash}`)
+}
+
+main()
+  .then(async () => {
+    console.log('All done!')
+  })
+  .catch(error => {
+    console.error(error)
+    process.exitCode = 1
+  })
