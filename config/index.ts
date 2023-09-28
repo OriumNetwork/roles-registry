5166e70f2ce6b4c99370544aa0856a7dd149ee30
Daniel Lima <daniel.lima@orium.network>
2023-09-28 10:14:19 -0300

    Deploy

--- config/index.ts
+++ config/index.ts
@@ -1,5 +1,5 @@
 export const DeployAddresses = {
   ImmutableCreate2Factory: '0x0000000000FFe8B47B3e2130213B802212439497',
-  RolesRegistry: '0x8680fC3Df31805B1F7f8BfB702f723460d360FE9',
+  RolesRegistry: '0x4c7E4a30a749d78935ED7674590026f7b74AFea0',
   KMSDeployer: '0x04c8c6c56dab836f8bd62cb6884371507e706806',
 }
