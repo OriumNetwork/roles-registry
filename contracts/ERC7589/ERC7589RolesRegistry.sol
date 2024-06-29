84b8270771ae80b0f9aab76d60bfe458b84718f2
Ernani São Thiago <ernanirst@gmail.com>
2024-06-29 17:22:15 -0400

    ON-876: removed unused extension

--- contracts/ERC7589/ERC7589RolesRegistry.sol
+++ contracts/ERC7589/ERC7589RolesRegistry.sol
@@ -7,7 +7,6 @@ import { IERC1155Receiver } from '@openzeppelin/contracts/token/ERC1155/IERC1155
 import { ERC1155Holder, ERC1155Receiver } from '@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol';
 import { IERC7589 } from '../interfaces/IERC7589.sol';
 import { IERC1155 } from '@openzeppelin/contracts/token/ERC1155/IERC1155.sol';
-import { IERC7589RoleBalanceOfExtension } from '../interfaces/IERC7589RoleBalanceOfExtension.sol';
 import { IERC7589LockTokensAndGrantRoleExtension } from '../interfaces/IERC7589LockTokensAndGrantRoleExtension.sol';
 import { Uint64SortedLinkedListLibrary } from '../libraries/Uint64SortedLinkedListLibrary.sol';
 import { IOriumWrapperManager } from '../interfaces/IOriumWrapperManager.sol';
